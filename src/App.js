daa68b9099ffbbdc13b824bc79b45e8ceb3f0e27
Julius D <30121440+julius-d@users.noreply.github.com>
2017-11-07 22:22:36 +0100

    remove react logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -41,7 +40,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Zufallszahlengeneratort</h1>
         </header>
         <div className="App-intro">
