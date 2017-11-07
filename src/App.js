3ecf4f5aa586b4dd124111486edfd4b50f8f2c6f
Julius D <30121440+julius-d@users.noreply.github.com>
2017-11-07 21:38:20 +0100

    basic version of random generator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      randomNumbers: [],
+      minValue: 1,
+      maxValue: 32
+    }
+  }
+
+  static getRandomInt(min, max) {
+    max = max + 1;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  generateRandomNumbers() {
+    const newRandom = App.getRandomInt(this.state.minValue, this.state.maxValue);
+    this.setState({
+      randomNumbers: [newRandom, ...this.state.randomNumbers]
+    });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: parseInt(value, 10)
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Zufallszahlengeneratort</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <div>
+            <label>Min</label>
+            <input name="minValue" type="number" value={this.state.minValue}
+                   onChange={this.handleInputChange.bind(this)}/>
+          </div>
+          <div>
+            <label>Max</label>
+            <input name="maxValue" type="number" value={this.state.maxValue}
+                   onChange={this.handleInputChange.bind(this)}/>
+          </div>
+          <button onClick={this.generateRandomNumbers.bind(this)}>Würfeln</button>
+          {this.state.randomNumbers.map((randomNumber, i) => <div key={i}><strong>{randomNumber}</strong></div>)}
+        </div>
       </div>
     );
   }
