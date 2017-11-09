96d5a9e145c336601756173ee028932a98cb3b05
Julius D <30121440+julius-d@users.noreply.github.com>
2017-11-09 20:59:31 +0100

    made the key value stable

--- src/resultList/ResultList.js
+++ src/resultList/ResultList.js
@@ -4,9 +4,10 @@ import React, {Component} from 'react';
 export default class ResultList extends Component {
 
   render() {
+    const numberOfResults = this.props.randomNumbers.length;
     return (
       <div className="Results">
-        {this.props.randomNumbers.map((randomNumber, i) => <div key={i}><strong>{randomNumber}</strong></div>)}
+        {this.props.randomNumbers.map((randomNumber, i) => <div key={numberOfResults-i}> <strong>{randomNumber}</strong></div>)}
       </div>
     );
 
