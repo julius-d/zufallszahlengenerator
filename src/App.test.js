251e6dc6e57057fe366df1b1e1c28aff9f982c2f
Julius D <30121440+julius-d@users.noreply.github.com>
2017-11-09 22:03:10 +0100

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 describe("App", () => {
 
@@ -9,6 +10,16 @@ describe("App", () => {
       <App/>
     ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('creates random numbers', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find("ResultList").props().randomNumbers.length).toEqual(0);
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find("ResultList").props().randomNumbers.length).toEqual(1);
+    wrapper.find('button').simulate('click');
+    expect(wrapper.find("ResultList").props().randomNumbers.length).toEqual(2);
+
   });
 
 });
